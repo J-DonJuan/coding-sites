00df66b6e75b8940dd50d76c66f4143ff0aa86af
J-DonJuan <jcdonnelly48@gmail.com>
2022-03-21 20:25:32 -0700

    pushing through CodeAcademy meal maker

--- codewars/script.js
+++ codewars/script.js
@@ -18,14 +18,27 @@ let menu = {
     set desserts(newDessert){
        this._courses._desserts.push(newDessert);
     },
+    get appetizers() {
+        console.log("--appetizers getter method incoming--")
+        console.log(this._courses._appetizers);
+    },
+    get mains() {
+        console.log("--mains getter method incoming--")
+        console.log(this._courses._mains);
+    },
+    get desserts() {
+        console.log("--desserts getter method incoming--")
+        console.log(this._courses._desserts);
+    },
 
     addDishToCourse: function(courseName, dishName, dishPrice){
         let dish = {
             [dishName]: dishPrice
         }
-        console.log(`dish key: ${Object.keys(dish)}`);
-        console.log(`dishPrice: ${dish[dishName]}`);
-        console.log(`trying to access courses: ${this._courses[courseName]}`)
+        // Just trying to figure out indexing in the object
+        // console.log(`dish key: ${Object.keys(dish)}`);
+        // console.log(`dishPrice: ${dish[dishName]}`);
+        // console.log(`trying to access courses: ${this._courses[courseName]}`)
         this._courses[courseName].push(dish);
 
     }
@@ -34,26 +47,15 @@ let menu = {
 
 menu.appetizers = "Mozzerella Sticks";
 menu.appetizers = "Onion Rings";
-console.log(menu._courses._appetizers);
+menu.appetizers;
 menu.mains = "Steak";
 menu.mains = "Eggs";
 menu.desserts = "Cake";
 menu.desserts = "Ice Cream";
-menu.courses;
 
 menu.addDishToCourse("_appetizers", "Extra Mozzerella Sticks", 5.99);
 menu.addDishToCourse("_mains", "Pork Chops", 18.99);
-menu.courses;
+
 // menu.courses;
-
-
-// // Testing adding strings to newly formed objects
-// string1 = "Appetizers";
-// string2 = "Mozzerella Sticks";
-
-// let dish = {
-//     [string1] : string2
-// }
-
-// // console.log(`dish.string1 = ${dish.string1}, dish.string2 = ${dish.string2}`);
-// console.log(dish);
+menu.appetizers;
+menu.desserts;
