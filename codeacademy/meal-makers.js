7f2e2562572cc3998b47ec65179f9360e9ee5e6f
J-DonJuan <jcdonnelly48@gmail.com>
2022-03-29 13:44:38 -0700

    Cleaning up

--- codeacademy/meal-makers.js
+++ codeacademy/meal-makers.js
@@ -65,3 +65,92 @@ let menu = {
   console.log(menu.mains);
   console.log(menu.getRandomDishFromCourse("mains"));
   menu.generateRandomMeal();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// let menu = {
+//     _courses: {
+//       _appetizers: [],
+//       _mains: [],
+//       _desserts: [],
+//     },
+//     get courses(){
+//         for (course in this._courses){
+
+//             // console.log(this._courses[course]);
+//         }
+//     },
+//     set appetizers(newAppetizer){
+//        this._courses._appetizers.push(newAppetizer);
+//     },
+//     set mains(newMain){
+//        this._courses._mains.push(newMain);
+//     },
+//     set desserts(newDessert){
+//        this._courses._desserts.push(newDessert);
+//     },
+//     get appetizers() {
+//         console.log("--appetizers getter method incoming--")
+//         console.log(this._courses._appetizers);
+//     },
+//     get mains() {
+//         console.log("--mains getter method incoming--")
+//         console.log(this._courses._mains);
+//     },
+//     get desserts() {
+//         console.log("--desserts getter method incoming--")
+//         console.log(this._courses._desserts);
+//     },
+
+//     addDishToCourse: function(courseName, dishName, dishPrice){
+//         let dish = {
+//             [dishName]: dishPrice
+//         }
+//         this._courses[courseName].push(dish);
+//     }
+// }
+
+
+// menu.addDishToCourse("_appetizers", "Onion Rings", 6.99);
+// menu.addDishToCourse("_appetizers", "Extra Mozzerella Sticks", 5.99);
+// menu.addDishToCourse("_mains", "Pork Chops", 18.99);
+// menu.addDishToCourse("_mains", "Steak", 24.99);
+
+// // menu.courses;
+// menu.appetizers;
+// menu.mains;
