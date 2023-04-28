0bbcc017b167353cc1aeb0e94668f3154b2fe51e
J-DonJuan <jcdonnelly48@gmail.com>
2023-04-27 23:08:50 -0700

    Implemented case check in ascii to handle non letter characters

--- codewars/kyu5/rot13.js
+++ codewars/kyu5/rot13.js
@@ -5,37 +5,42 @@ Create a function that takes a string and returns the string ciphered with Rot13
 If there are numbers or special characters included in the string, they should be returned as they are. 
 Only letters from the latin/english alphabet should be shifted, like in the original Rot13 "implementation". */
 
+const generateAlphabet = () => {
+    const alphabetArray = [];
+    for (let i = 97; i <= 122; i++) {
+        alphabetArray.push(String.fromCharCode(i));
+    };
+    return alphabetArray;
+}
 
+// Generate global alphabet array
+let alphabet = generateAlphabet();
+
+const reIndex = char => {
+    let index = alphabet.indexOf(char.toLowerCase());
+    index += 13;
+    index %= 26;
+    return index;
+}
 
 const rot13 = message => {
-    // Create an alphabet array
-    const alphabet = [];
-    for (let i = 97; i <= 122; i++) {
-        alphabet.push(String.fromCharCode(i));
-    };
-
     let resultArray = [];
 
     for (char of message){
-        if (char === char.toLowerCase()){
-            let index = alphabet.indexOf(char);
-            index += 13;
-            index %= 26;
-            resultArray.push(alphabet[index]);
+        if (char.charCodeAt(0) >= 97 && char.charCodeAt(0) <= 122){
+            resultArray.push(alphabet[reIndex(char)]);
+        } else if (char.charCodeAt(0) >= 65 && char.charCodeAt(0) <= 90){
+            resultArray.push(alphabet[reIndex(char)].toUpperCase());
         } else {
-            let index = alphabet.indexOf(char.toLowerCase());
-            index += 13;
-            index %= 26;
-            resultArray.push(alphabet[index].toUpperCase());
+            resultArray.push(char);
         }
-
     }
     return resultArray.join('');
 }
 
 
 
-str = "abcdz";
+str = "aB Cdz!!";
 console.log(rot13(str));
 
 
